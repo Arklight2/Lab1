58656126983b0e1e348b6490b9d1644fdb42184a
Arklight2 <160391803+Arklight2@users.noreply.github.com>
2025-02-09 19:10:37 +0300

    Список изменений

---
+++ Z4/Z4.py
@@ -0,0 +1,8 @@
+def sort_russian_flag(colors):
+    order = {"белый": 0, "синий": 1, "красный": 2}
+    return sorted(colors, key=lambda color: order.get(color, 3))
+
+# Пример массива цветов
+colors = ["красный", "белый", "синий"]
+sorted_colors = sort_russian_flag(colors)
+print("Упорядоченные цвета флага:", sorted_colors)
