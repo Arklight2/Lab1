95ae21e2091ee643c7ab7530462e072227f33856
Arklight2 <160391803+Arklight2@users.noreply.github.com>
2025-02-09 16:22:24 +0300

    Список изменений

--- Z2/Z2.py
+++ Z2/Z2.py
@@ -1,1 +1,11 @@
-print(1)
+import random
+
+def shuffle_words(sentence):
+    words = sentence.split()
+    random.shuffle(words)
+    return ' '.join(words)
+
+
+sentence = input("Введите строку: ")
+shuffled_sentence = shuffle_words(sentence)
+print("Перемешанная строка:", shuffled_sentence)
